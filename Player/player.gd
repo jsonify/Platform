e9e13e4512eff980dd87ab6791a8e41a9bdf10e2
Jason <phlacin@gmail.com>
2023-03-03 16:43:32 -0800

    Got some coded FSM done, new hero now

--- Player/player.gd
+++ Player/player.gd
@@ -11,52 +11,107 @@ extends CharacterBody2D
 @export var THRUST := -1
 @export var MAX_THRUST := 50
 
-@onready var animated_sprite := $AnimatedSprite2D
+#@onready var animated_sprite := $AnimatedSprite2D
 @onready var animation_player := $AnimationPlayer
 
-enum state {
+enum {
+	IDLE,
 	RUN,
 	JUMP,
 	FALL,
-	IDLE
+	FLYING
 }
+
+var state = FALL
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	apply_gravity()
 	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if Input.is_action_pressed("ui_up"):
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
+	
+	match state:
+		IDLE:
+			idle_state(input)
+		RUN:
+			run_state(input)
+		JUMP:
+			jump_state(input)
+		FALL:
+			fall_state()
+		FLYING:
+			flying_state(input)
+	
+#
+#	if Input.is_action_pressed("ui_up"):
+#		apply_thrust()
 		
-		apply_thrust()
-		
-	if input.x == 0:
-		animated_sprite.play("Idle")
-		apply_friction()
-	else:
-		animated_sprite.play("Run")
-		
-		apply_acceleration(input.x)
+#	if input.x == 0:
+#		animation_player.play("Idle")
+#		apply_friction()
+#	else:
+#		animation_player.play("Run")
+#
+#		apply_acceleration(input.x)
 		
 	
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
-	else:
-		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_RELEASE_FORCE:
-			velocity.y = JUMP_RELEASE_FORCE
-	fast_fall()
+#	if is_on_floor():
+#		animation_player.play("Idle")
+		
+#	else:
+#		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_RELEASE_FORCE:
+#			velocity.y = JUMP_RELEASE_FORCE
+#	fast_fall()
 	move_and_slide()
 
+func run_state(input):
+	apply_gravity()
+	print("Run state")
+	if Input.is_action_just_pressed("ui_accept"):
+		state = JUMP
+	
+func flying_state(input):
+	apply_gravity()
+	if Input.is_action_pressed("ui_up"):
+		apply_thrust()
+	else:
+		state = FALL
+	
+func jump_state(input):
+	apply_gravity()
+	velocity.y = JUMP_VELOCITY
+	if is_on_floor() and input.x == 0:
+		state = IDLE
+	elif is_on_floor() and input.x != 0:
+		state = RUN
+	
+func idle_state(input):
+	if input.x == 0:
+		animation_player.play("Idle")
+	elif Input.is_action_pressed("ui_up"):
+		state = FLYING
+	elif input.x != 0:
+		state = RUN
+	elif Input.is_action_just_pressed("ui_accept"):
+		state = JUMP
+
+func fall_state():
+	animation_player.play("Fall")
+	apply_gravity()
+	if Input.is_action_pressed("ui_up"):
+		state = FLYING
+	if is_on_floor():
+		state = IDLE
+
 func apply_thrust():
-#	animated_sprite.play("Fly")
+	animation_player.play("Fly")
 	velocity.y = lerp(0, MAX_THRUST, THRUST)
 
 func fast_fall():
 	if velocity.y > 0:
-#		animated_sprite.play("Fall")
+		animation_player.play("Fall")
 		velocity.y += ADDITIONAL_FALL_GRAVITY
 
 func apply_gravity():
@@ -67,15 +122,3 @@ func apply_acceleration(amount):
 
 func apply_friction():
 	velocity.x = move_toward(velocity.x, 0, FRICTION)
-	
-func run_state():
-	pass
-	
-func jump_state():
-	pass
-	
-func fall_state():
-	pass
-	
-func idle_state():
-	pass
