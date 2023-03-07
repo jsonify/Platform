e967ed6792a5601254c1710541acab1f242ef8ba
Jason <phlacin@gmail.com>
2023-03-06 16:29:53 -0800

    Finished basic flower_enemy move mechanic

--- Enemies/FlowerEnemy/flower_enemy.gd
+++ Enemies/FlowerEnemy/flower_enemy.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
-@onready var ledge_check := $LedgeCheck
+@onready var ledge_check = $LedgeCheck
+
 
 var direction := Vector2.RIGHT
 
 func _physics_process(delta):
+	velocity = direction * 10
+	handle_wall_or_ledge()
+	move_and_slide()
+
+func handle_wall_or_ledge():
 	var found_wall = is_on_wall()
-	var found_ledge = not ledge_check.is_colliding()
-	
+	var found_ledge = not ledge_check.is_colliding() 
 	if found_wall or found_ledge:
 		direction *= -1
-		
-	velocity = direction * 10
+		scale.x = -1
 	
-	
-	
-	move_and_slide()
