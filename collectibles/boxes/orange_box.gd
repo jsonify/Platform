e6c9a0f14056da876a040da701fae72d9df4c18f
Jason <phlacin@gmail.com>
2023-04-13 13:58:45 -0700

    basic with inherited purple and orange boxes created

---
+++ collectibles/boxes/orange_box.gd
@@ -0,0 +1,8 @@
+extends BoxBasic
+
+
+func _on_body_entered(body):
+		if body is Player:
+			animation_player.play("opening")
+			collision_shape.set_deferred("disabled", true)
+			print("orange box now here")
