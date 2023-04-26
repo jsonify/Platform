18dae8c3523399175797c40faa36942804be9085
Jason <phlacin@gmail.com>
2023-04-25 17:25:23 -0700

    creating custom HitBox

--- entities/enemies/flowery.gd
+++ entities/enemies/flowery.gd
@@ -13,7 +13,7 @@ func _on_detection_area_player_undetected(body):
 
 
 
-func hit_effect():
+func hit_effect(area):
 	speed = 0
 	animation_player.play("hit")
 	await animation_player.animation_finished
@@ -21,10 +21,10 @@ func hit_effect():
 	animation_player.play("death")
 	await animation_player.animation_finished
 	queue_free()
+	print("killed by " + area.name)
 	
 	
 
 
-
-func _on_hurt_box_area_entered(area):
-	hit_effect()
+func _on_hurt_box_take_damage(amount):
+	print("Just took " + str(amount) + " of damage")
--- overlaps/hurt_box.gd
+++ overlaps/hurt_box.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-signal give_damage(amount)
+signal take_damage(amount)
 
 const HIT_EFFECT = preload("res://effects/hit_effect.tscn")
 
